2a36f79183568f6a56a7d63655d8185937021049
maya-abdoh <mayaabdoh2002@gmail.com>
2023-07-27 16:06:35 -0700

    edit Task 25

--- Code Review Session/Task 25/ThisCode2Fixed.js
+++ Code Review Session/Task 25/ThisCode2Fixed.js
@@ -1,27 +1,29 @@
 function foodsAndDrinks() {
-    const drinks = ['lemonade', 'soda', 'tea', 'water'];
-    const foods = ['beans', 'chicken', 'rice'];
-  
-    for (let i = 0; i < drinks.length; i++) {
-      console.log(drinks[i]);
-    }
-  
-    for (let i = 0; i < foods.length; i++) {
-      console.log(foods[i]);
-    }
-  
-    return;
+  // Variables to store drinks and foods
+  const drinks = ['lemonade', 'soda', 'tea', 'water'];
+  const foods = ['beans', 'chicken', 'rice'];
+
+  // print drinks
+  console.log("Drinks:");
+  for (const drink of drinks) {
+    console.log(drink);
   }
 
-  function getAge() {
-    const birthDate = new Date(1985, 1, 16);
-    const now = new Date();
-    const age = now.getFullYear() - birthDate.getFullYear();
-    return age;
+  // print foods
+  console.log("Foods:");
+  for (const food of foods) {
+    console.log(food);
   }
-  
-  function PrintNumberAsWord(value) {
-    const words = ["zero", "one", "two", "three", "four", "five"];
-    return words[value] || "Invalid value"; 
-  }
-  
+}
+// calculate age
+function getAge() {
+  const birthDate = new Date(1985, 1, 16);
+  const now = new Date();
+  const age = now.getFullYear() - birthDate.getFullYear();
+  return age;
+}
+
+function printNumberAsWord(value) {
+  const words = ["zero", "one", "two", "Three", "Four", "Five"];
+  return words[value] || "Invalid value";
+}
--- Code Review Session/Task 25/ThisCode3Fixed.js
+++ Code Review Session/Task 25/ThisCode3Fixed.js
@@ -1,17 +1,38 @@
-function LogCustomerInfo(custCode, custName, custAge, custPhone, custMobile, custAddress, custEmail) {
-    console.log("cust code:", custCode);
-    console.log("cust name:", custName);
-    console.log("cust age:", custAge);
-    console.log("cust phone:", custPhone);
-    console.log("cust mobile:", custMobile);
-    console.log("cust address:", custAddress);
-    console.log("cust email:", custEmail);
+function logCustomerInfo(customerInfoString) {
+  try {
+    const customerInfo = JSON.parse(customerInfoString);
+    console.log("Customer Code:", customerInfo.custCode);
+    console.log("Customer Name:", customerInfo.custName);
+    console.log("Customer Age:", customerInfo.custAge);
+    console.log("Customer Phone:", customerInfo.custPhone);
+    console.log("Customer Mobile:", customerInfo.custMobile);
+    console.log("Customer Address:", customerInfo.custAddress);
+    console.log("Customer Email:", customerInfo.custEmail);
+  } catch (error) {
+    console.error("An error occurred while logging customer information:", error);
   }
-  
-  function MyFunction() {
-    const ZeadAge = 36;
-    const ahmadAge = 35;
-    const MohammedAge = 40;
-    alert("Zead Age is: " + ZeadAge);
+}
+function myFunction() {
+  const zeadAge = 36;
+  try {
+    alert("Zead Age is: " + zeadAge);
+  } catch (error) {
+    console.error("An error occurred while showing Zead's age:", error);
   }
-  
+}
+const customerInfoString = `{
+  "custCode": "C123",
+  "custName": "Maya abdoh",
+  "custAge": 21,
+  "custPhone": "123-456-7890",
+  "custMobile": "987-654-3210",
+  "custAddress": "123 salah al deen St",
+  "custEmail": "Maya@example.com"
+}`;
+
+try {
+  logCustomerInfo(customerInfoString);
+  myFunction();
+} catch (error) {
+  console.error("An unexpected error occurred:", error);
+}
