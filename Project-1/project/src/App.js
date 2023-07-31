e8a93a72b30f9ce3211d2130e98be25942100d2f
maya-abdoh <mayaabdoh2002@gmail.com>
2023-07-31 16:02:22 -0700

    edit

--- Project-1/project/src/App.js
+++ Project-1/project/src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PaperGroupContest.css';
-import StartPage from './StartPage';
-
 function getPaperGroupData() {
   const data = {
     category1: {
@@ -72,37 +70,70 @@ function getPaperGroupData() {
 }
 function PaperGroupContest() {
   const contestData = getPaperGroupData();
+  const [currentCategoryKey, setCurrentCategoryKey] = useState(Object.keys(contestData)[0]);
+  const categoryKeys = Object.keys(contestData);
+  const category = contestData[currentCategoryKey];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < category.questions.length - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    } else {
+      const currentIndex = categoryKeys.indexOf(currentCategoryKey);
+      if (currentIndex < categoryKeys.length - 1) {
+        setCurrentCategoryKey(categoryKeys[currentIndex + 1]);
+        setCurrentQuestionIndex(0);
+      }
+    }
+  };
+
+  const handlePreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+    } else {
+      const currentIndex = categoryKeys.indexOf(currentCategoryKey);
+      if (currentIndex > 0) {
+        setCurrentCategoryKey(categoryKeys[currentIndex - 1]);
+        const prevCategory = contestData[categoryKeys[currentIndex - 1]];
+        setCurrentQuestionIndex(prevCategory.questions.length - 1);
+      }
+    }
+  };
+
+  const isLastQuestion = currentQuestionIndex === category.questions.length - 1;
+  const isFirstQuestion = currentQuestionIndex === 0;
+  const currentQuestion = category.questions[currentQuestionIndex];
 
   return (
-    <div>
-      <StartPage />
     <div className="contest-container">
-      {Object.keys(contestData).map((categoryKey) => {
-        const category = contestData[categoryKey];
-        return (
-          <div key={categoryKey}>
-            <h2>{category.categoryTitle}</h2>
-            {category.questions.map((question, index) => (
-              <div key={index}>
-                <p>{question.question}</p>
-                <div className="answer-grid">
-                  <div className="answer-row">
-                    <button>{question.answer1}</button>
-                    <button>{question.answer3}</button>
-                  </div>
-                  <div className="answer-row">
-                    <button >{question.answer2}</button>
-                    <button>{question.answer4}</button>
-                  </div>
-                </div>
-              </div>
-            ))}
+      <h2>{category.categoryTitle}</h2>
+      <div>
+        <p>{currentQuestion.question}</p>
+        <div className="answer-grid">
+          <div className="answer-row">
+            <button>{currentQuestion.answer1}</button>
+            <button>{currentQuestion.answer3}</button>
           </div>
-        );
-      })}
-    </div></div>
+          <div className="answer-row">
+            <button>{currentQuestion.answer2}</button>
+            <button>{currentQuestion.answer4}</button>
+          </div>
+        </div>
+      </div>
+      <div className="navigation-buttons">
+        {currentQuestionIndex === 0 && currentCategoryKey !== categoryKeys[0] && (
+          <button onClick={handlePreviousQuestion}>Previous Question</button>
+        )}
+        {!isFirstQuestion && (
+          <button onClick={handlePreviousQuestion}>Previous Question</button>
+        )}
+        <button onClick={handleNextQuestion}>
+          {isLastQuestion && currentCategoryKey !== categoryKeys[categoryKeys.length - 1]
+            ? 'Next Question'
+            : 'Next Question'}
+        </button>
+      </div>
+    </div>
   );
 }
+
 export default PaperGroupContest;
-
-
