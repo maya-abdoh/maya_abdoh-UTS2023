32c275f5273962f740b246c2f8431bcf66c63db9
Ayaj003 <aya.ja@outlook.com>
2023-08-02 01:33:19 +0300

    Changing Theme and questions design

--- Project-1/project/src/StartPage.js
+++ Project-1/project/src/StartPage.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StartPage.css";
 
-export default class StartPage extends React.Component {
+function StartPage(props) {
+  const { theme, onThemeChange } = props;
+  const [startClicked, setStartClicked] = useState(false);
 
-  render() {
-    return (
-      <div className="card">
-         <b>شارك الآن</b>
-         <p> هل أنتم مستعدون لاختبار معارفكم وتحدي ذكائكم؟ انضموا إلينا في مسابقة ممتعة ومثيرة تتيح لكم فرصة الفوز بجوائز قيمة! ستتاح لكم فرصة اختبار معلوماتكم حول التاريخ الوطني والثقافة الدينية لبلدنا الحبيب
-         </p>
-         <button className="start">ابدأ</button>
-        </div>
-    );
+  const handleStartClick = () => {
+    setStartClicked(true);
+    props.onStartClick();
+  };
+
+  if (startClicked) {
+    return null;
   }
+
+  return (
+    <div className={theme}>
+      <button className="theme" onClick={onThemeChange}>Change Theme</button>
+      <div className="card">
+        <b>شارك الآن</b>
+        <p>
+          هل أنتم مستعدون لاختبار معارفكم وتحدي ذكائكم؟ انضموا إلينا في مسابقة
+          ممتعة ومثيرة تتيح لكم فرصة الفوز بجوائز قيمة! ستتاح لكم فرصة اختبار
+          معلوماتكم حول التاريخ الوطني والثقافة الدينية لبلدنا الحبيب
+        </p>
+        <button className="start" onClick={handleStartClick}>
+          ابدأ
+        </button>
+      </div>
+    </div>
+  );
 }
+
+export default StartPage;
